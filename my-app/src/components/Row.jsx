e4a4a59ce0f99f80edb8822269c4413c9411ee0e
abhikanakavalli <abhikanakavalli98@gmail.com>
2023-03-28 00:07:31 +0530

    3 commit firebase auth setup

--- my-app/src/components/Row.jsx
+++ my-app/src/components/Row.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import Movie from './Movie';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
-const Row = ({title, fetchURL}) => {
+const Row = ({title, fetchURL, rowId}) => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -10,24 +12,33 @@ const Row = ({title, fetchURL}) => {
         })
     }, [fetchURL])
 
-    console.log('rows', movies);
+    const slideLeft = () => {
+        let slider = document.getElementById('slider' + rowId);
+        slider.scrollLeft = slider.scrollLeft - 500 
+    }
+
+    const slideRight = () => {
+        let slider = document.getElementById('slider' + rowId);
+        slider.scrollLeft = slider.scrollLeft + 500 
+    }   
 
   return (
     <>
     <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-    <div className='relative flex items-center'>
-        <div id={'slider'}>
+    <div className='relative flex items-center group'>
+        <MdChevronLeft
+            onClick={slideLeft}
+            className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+        <div id={'slider' + rowId} 
+            className='w-full h-full 
+        overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
             {movies.map((item, id) => (
-                <div 
-                    className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
-                >
-                    <img className='w-full h-auto block' w-25 src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
-                    <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                        <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
-                    </div>
-                </div>
+                <Movie key={id} item={item}/>
             ))}
         </div>
+        <MdChevronRight
+            onClick={slideRight}
+            className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
     </div>
     </>
   )
---
+++ my-app/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_gHvX_0nGR7CAeU69ynAGE6NQOueR5sA",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
